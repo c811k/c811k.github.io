d3048528f4ae7319c35c0b86d80f7ab94034fc0b
Caleb Kang <bhk811@gmail.com>
2019-12-08 21:01:47 -0800

    aboutme update

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 var prevScrollpos = window.pageYOffset;
-
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    navbar.classList.add("shadow");
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 }
 
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+$(".snake").snakeify({
+  speed: 200
+});
