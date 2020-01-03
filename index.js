0b46634b273031fa81ef3b7cc03b1e11bb772b64
Caleb Kang <bhk811@gmail.com>
2020-01-02 16:32:19 -0800

    about me

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-    navbar.classList.add("shadow");
+    navbar.classList.add("shadow-sm");
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
